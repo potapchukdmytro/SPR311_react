20b76285fa55b308cee1f87f383adb662fdd6e73
Dmytro Potapchuk <traiget@gmail.com>
2024-07-20 12:14:50 +0300

    first commit

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import logo from "./logo.svg";
+import "./App.css";
+import Navbar from "./components/Navbar";
+import Footer from "./components/Footer";
+
+function App() {
+    return (
+        <>
+            <Navbar />
+            <img src={logo}/>
+            <Footer />
+        </>
+    );
+}
+
+export default App;
---
+++ src/components/Navbar.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+function Navbar() {
+
+    const variable = 20;
+    console.log(variable);
+
+    return (
+        <h1>Navbar</h1>
+    );
+};
+
+export default Navbar;
