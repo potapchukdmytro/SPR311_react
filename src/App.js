16da3ae75fca09baed7f727ae011f5ad4b86d038
Dmytro Potapchuk <traiget@gmail.com>
2024-10-12 13:12:21 +0300

    added user geolocation

--- src/App.js
+++ src/App.js
@@ -16,6 +16,7 @@ import { ThemeProvider } from "@mui/material";
 import { darkTheme, lightTheme } from "./theming/themes";
 import { useSelector } from "react-redux";
 import "./localization/config";
+import axios from "axios";
 
 
 function App() {
@@ -29,6 +30,38 @@ function App() {
     let currentTheme = lightTheme;
     if(isDark) {
         currentTheme = darkTheme;
+    }
+
+    // user location
+    navigator.geolocation.getCurrentPosition(getLocationSuccess, getLocationError);
+
+    function getLocationSuccess(data) {
+        const lon = data.coords.longitude;
+        const lat = data.coords.latitude;
+
+        const apiLocationUrl = `https://api.openweathermap.org/geo/1.0/reverse?appid=5a31fbfd92c0522d248db2c164ac47c5&lat=${lat}&lon=${lon}`;
+        axios.get(apiLocationUrl).then(response => {            
+            const {data} = response;
+            const city = data[0].local_names.uk;
+            console.log(city);
+        });
+    }
+
+    
+    function getLocationError(error) {
+        getLocationByUserIP();
+    }
+
+    function getLocationByUserIP() {
+        // axios.get("https://api.ipify.org/?format=json")
+        // .then(response => {
+        //     console.log(response.data.ip);
+        // });
+
+        axios.get("https://api.geoapify.com/v1/ipinfo?&apiKey=5a99905f68e54e7fb672817b26689a02")
+        .then(response => {
+            console.log(response.data.city.name);
+        });
     }
 
     return (
